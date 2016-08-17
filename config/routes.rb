807ac4fa88bb59521df035f99828404288a7e90e
thomasjbhamilton <thomas@reverbconsulting.com>
2016-08-17 16:00:35 -0400

    refactor

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
   root to: 'movies#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html'
